93f4b6d2d4c510308f4ab3e690eb69d99c918ade
Angus Fisk <angus.fisk@ndcn.ox.ac.uk>
2018-11-14 21:37:40 +0200

    Updates for all delta hypnogram working perfectly!

--- 02_plot_delta_hypnograms.py
+++ 02_plot_delta_hypnograms.py
@@ -1,8 +1,6 @@
 # script to run to check remove header working as expected
 import pathlib
 import sys
-import matplotlib.pyplot as plt
-import numpy as np
 sys.path.insert(0, "/Users/angusfisk/Documents/01_PhD_files/"
                    "07_python_package/sleepPy")
 import sleepPy.preprocessing as prep
@@ -13,29 +11,39 @@ sns.set()
 
 # define import dir
 input_dir = pathlib.Path("/Users/angusfisk/Documents/01_PhD_files/01_projects"
-                         "/P3_LLEEG_Chapter3/01_data_files/07_clean_fft"
-                         "/01_reindexed")
-save_dir = input_dir.parents[2] / "03_analysis_outputs"
+                         "/P3_LLEEG_Chapter3/01_data_files/07_clean_fft")
+save_dir = input_dir.parents[1] / "03_analysis_outputs"
 subdir_name = "01_delta_hypnograms"
 
-test_object = prep.SaveObjectPipeline(input_directory=input_dir,
+read_kwargs = {
+    "index_col":[0,1],
+    "header":[0]
+}
+
+hypnogram_object = prep.SaveObjectPipeline(input_directory=input_dir,
                                       save_directory=save_dir,
                                       read_file_fx=(prep,
                                                     "read_file_to_df"),
-                                      index_col=[0],
-                                      header=[0],
-                                      drop_cols=False,
-                                      rename_cols=False,
-                                      check_cols=False)
+                                      **read_kwargs)
 
+plot_kwargs = {
+    "data_list":None,
+    "showfig":False,
+    "savefig":True,
+    "figsize":(10,10),
+    "name_of_band":["Delta"],
+    "range_to_sum":("0.50Hz", "4.00Hz"),
+    "level_of_index":0,
+    "label_col":-1,
+    "base_freq":"4S",
+    "plot_epochs":False,
+    "set_file_title":True,
+    "sharey":False,
+    "legend":False
+}
 
-test_object.create_plot(function_name="plot_spectral_hypnogram",
-                        module=plot,
-                        subdir_name=subdir_name,
-                        data_list=test_object.df_list,
-                        remove_col=False,
-                        spectrum_name=["delta"],
-                        spectrum_range=("0.50Hz", "4.00Hz"),
-                        savefig=True,
-                        figsize=(20,10))
-                        
+hypnogram_object.create_plot(function=(plot, "plot_hypnogram_from_df"),
+                             subdir_name=subdir_name,
+                             remove_col=False,
+                             **plot_kwargs)
+                            
