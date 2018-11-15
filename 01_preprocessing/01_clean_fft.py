b9bd37952644cfc07f3549ad1a73e0d998490705
Angus Fisk <angus.fisk@ndcn.ox.ac.uk>
2018-11-15 12:47:27 +0200

    updates to have to clean files all working to create single file for each animal

--- 01_preprocessing/01_clean_fft.py
+++ 01_preprocessing/01_clean_fft.py
@@ -6,33 +6,37 @@ sys.path.insert(0, "/Users/angusfisk/Documents/01_PhD_files/07_python_package/"
                 "sleepPy")
 import sleepPy.preprocessing as prep
 
-input_directory = pathlib.Path("/Users/angusfisk/Documents/01_PhD_files/"
+input_dir = pathlib.Path("/Users/angusfisk/Documents/01_PhD_files/"
                                "01_projects/P3_LLEEG_Chapter3/01_data_files/"
                                "06_fft_files")
                               
-save_directory = input_directory.parent
+save_dir = input_dir.parent
 subdir_name = "07_clean_fft"
 
-clean_object = prep.SaveObjectPipeline(input_directory=input_directory,
-                                       save_directory=save_directory,
-                                       search_suffix=".txt",
-                                       readfile=False)
+clean_object = prep.SaveObjectPipeline(input_directory=input_dir,
+                                      save_directory=save_dir,
+                                      search_suffix=".txt",
+                                      readfile=False)
 
+animal_file_list = prep.create_dict_of_animal_lists(clean_object.file_list,
+                                                    input_dir,
+                                                    anim_range=(0,3))
+                                                    
 kwargs = {
-      "save_suffix_file":"_clean.csv",
-      "savecsv":True,
-      "object_list":clean_object.file_list,
-      "header":17,
-      "derivation_list":["fro", "occ", "foc"],
-      "der_label":"Derivation",
-      "time_index_column":(2),
-      "test_index_range":[0,1,2,-2,-1]
-  }
+    "save_suffix_file":"_clean.csv",
+    "savecsv":True,
+    "function":(prep, "single_df_for_animal"),
+    "subdir_name":subdir_name,
+    "object_list":animal_file_list.values(),
+    "file_list":animal_file_list.keys(),
+    "header":17,
+    "derivation_list":["fro", "occ", "foc"],
+    "der_label":"Derivation",
+    "time_index_column":(2),
+    "test_index_range":[0,1,2,-2,-1],
+    "period_label":"Light_period",
+    "anim_range":(0,3),
+    "day_range":(-6)
+}
 
-clean_object.process_file(module=prep,
-                          function_name="read_clean_fft_file",
-                          subdir_name=subdir_name,
-                          **kwargs)
-
-
-
+clean_object.process_file(**kwargs)
