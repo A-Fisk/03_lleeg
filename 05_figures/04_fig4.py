c51eb9247411fb7ef461e02c3001d4c7d0d7c806
Angus Fisk <angus.fisk@bnc.ox.ac.uk>
2019-05-16 09:45:03 +0100

    New version Figure 4.
    Removes regressions
    Adds in grid lines for spectrum
    Adds stats for all plots
    Adds in stats bar for count subfigure

--- 05_figures/04_fig4.py
+++ 05_figures/04_fig4.py
@@ -21,6 +21,7 @@ import sleepPy.plots as plot
 sys.path.insert(0, "/Users/angusfisk/Documents/01_PhD_files/"
                    "07_python_package/actiPy")
 import actiPy.episodes as ep
+import actiPy.preprocessing as aprep
 
 # define constants
 INDEX_COLS = [0, 1, 2]
@@ -78,9 +79,14 @@ zt_slice = spectrum_df.loc[
 nrem_mask = zt_slice["Stage"] == "NR"
 nrem_slice = zt_slice.where(nrem_mask).drop("Stage", axis=1)
 nrem_mean = nrem_slice.groupby(level=[0, 1, 2]).mean()
-long_nrem = nrem_mean.iloc[:, 3:].stack().reset_index()
+nrem_mean_mean = nrem_mean.groupby(level=1).mean()
+nrem_mean_sem = nrem_mean.groupby(level=1).sem()
+long_nrem = nrem_mean_mean.iloc[:, 3:].stack().reset_index()
 cols = ["Animal", "Light_period", "Derivation", "Frequency", "Power"]
-long_nrem.columns = cols
+long_nrem_cols = [cols[x] for x in [1, 3, 4]]
+long_nrem.columns = long_nrem_cols
+long_nrem["SEM"] = nrem_mean_sem.iloc[:, 3:].stack().values
+
 
 # Step 4 Calculate episodes of activity per hour
 
@@ -111,7 +117,7 @@ wake_episodes_df = wake_int_df.groupby(
 )
 
 # Get the number of episodes using sum resample
-nr_ep_mask = (wake_episodes_df > 1).astype(bool)
+nr_ep_mask = (nr_episodes_df > 1).astype(bool)
 nr_ep_sum = nr_ep_mask.groupby(
     level=0
 ).resample(
@@ -122,7 +128,7 @@ nr_ep_sum = nr_ep_mask.groupby(
 long_sum = nr_ep_sum.stack().reset_index()
 
 # get the mean duration using mean
-nr_ep_mean = wake_episodes_df.groupby(
+nr_ep_mean = nr_episodes_df.groupby(
     level=0
 ).resample(
     "H",
@@ -140,180 +146,184 @@ frag_cols = [
 long_frag.columns = frag_cols
 long_frag["Mean"] = long_mean.iloc[:, -1]
 
-# Step 5 Calculate delta SWA for NREM and wake
-
-# Calculate SWA / hour
-# Get sum and mean of artefact free delta power of a single derivation
-band = ["Delta"]
-range_to_sum = ("0.50Hz", "4.00Hz")
-der = "fro"
-swa_df = prep.create_df_for_single_band(
-    spectrum_df,
-    name_of_band=band,
-    range_to_sum=range_to_sum,
-    sum=False,
-    mean=True
-)
-nrem_mask = swa_df["Stage"].isin(["NR"])
-swa_nrem = swa_df.where(          # select only artefact free NREM
-    nrem_mask,
-    other=np.nan
-).loc[
-    idx[:, :, der, :],
-    band
-]
-
-# swa_nrem = swa_nrem.loc[idx["LL2", :, :, :], :] # Hack LL1 values for now
-
-swa_hr = swa_nrem.groupby(        # Resample to hourly
-    level=[0, 1]
-).resample(
-    "H",
-    level=3,
-    loffset=OFFSET,
-).mean()
-
-def norm_base_mean(protocol_df, baseline_str: str = "Baseline_0"):
-    base_values = protocol_df.loc[idx[:, baseline_str], :]
-    normalise_value = base_values.mean()
-    normalised_df = (protocol_df / normalise_value) * 100
-    return normalised_df
-
-swa_hr_norm = swa_hr.groupby(
-    level=[0]
-).apply(
-    norm_base_mean
-)
-
-swa_hr_shift_plus = swa_hr_norm.shift(-1)
-swa_hr_shift_minus = swa_hr_norm.shift(1)
-swa_hr_delta = ((swa_hr_shift_plus - swa_hr_shift_minus))# / swa_hr_norm) * 100
-# swa_hr_delta_mean = swa_hr_delta.groupby(level=[1, 2]).mean()
-swa_hr_delta_long = swa_hr_delta.reset_index()
-
-# Calculate NREM / hour
-nrem_stages = ["NR", "N1"]
-nrem_int_df = stage_df.isin(nrem_stages).astype(int)
-nr_hourly_perc = nrem_int_df.groupby(
-    level=0
-).resample(
-    "H",
-    level=1,
-    loffset=OFFSET
-).mean() * 100
-# nr_hourly_perc_mean = nr_hourly_perc.groupby(level=[1]).mean()
-nr_hourly_perc_long = nr_hourly_perc.stack(
-).reorder_levels(
-    [0, 2, 1]
-).sort_index(
-).reset_index()
-
-# Calculate Wake / hour
-wake_stages = ["W", "W1"]
-wake_int_df = stage_df.isin(wake_stages).astype(int)
-wake_hourly_perc = wake_int_df.groupby(
-    level=0
-).resample(
-    "H",
-    level=1,
-    loffset=OFFSET
-).mean() * 100
-# wake_hourly_perc_mean = wake_hourly_perc.groupby(level=[1]).mean()
-wake_hourly_perc_long = wake_hourly_perc.stack(
-).reorder_levels(
-    [0, 2, 1]
-).sort_index(
-).reset_index()
-
-# Regression NREM ~ delta swa
-swa_diss_df = swa_hr_delta_long.copy()
-swa_diss_df["NR"] = nr_hourly_perc_long[nr_hourly_perc_long.columns[-1]]
-
-# Regression wake ~ delta swa
-swa_accum_df = swa_hr_delta_long.copy()
-swa_accum_df["Wake"] = wake_hourly_perc_long[wake_hourly_perc_long.columns[-1]]
+# # Step 5 Calculate delta SWA for NREM and wake
+#
+# # Calculate SWA / hour
+# # Get sum and mean of artefact free delta power of a single derivation
+# band = ["Delta"]
+# range_to_sum = ("0.50Hz", "4.00Hz")
+# der = "fro"
+# swa_df = prep.create_df_for_single_band(
+#     spectrum_df,
+#     name_of_band=band,
+#     range_to_sum=range_to_sum,
+#     sum=False,
+#     mean=True
+# )
+# nrem_mask = swa_df["Stage"].isin(["NR"])
+# swa_nrem = swa_df.where(          # select only artefact free NREM
+#     nrem_mask,
+#     other=np.nan
+# ).loc[
+#     idx[:, :, der, :],
+#     band
+# ]
+#
+# # swa_nrem = swa_nrem.loc[idx["LL2", :, :, :], :] # Hack LL1 values for now
+#
+# swa_hr = swa_nrem.groupby(        # Resample to hourly
+#     level=[0, 1]
+# ).resample(
+#     "H",
+#     level=3,
+#     loffset=OFFSET,
+# ).mean()
+#
+# def norm_base_mean(protocol_df, baseline_str: str = "Baseline_0"):
+#     base_values = protocol_df.loc[idx[:, baseline_str], :]
+#     normalise_value = base_values.mean()
+#     normalised_df = (protocol_df / normalise_value) * 100
+#     return normalised_df
+#
+# swa_hr_norm = swa_hr.groupby(
+#     level=[0]
+# ).apply(
+#     norm_base_mean
+# )
+#
+# swa_hr_shift_plus = swa_hr_norm.shift(-1)
+# swa_hr_shift_minus = swa_hr_norm.shift(1)
+# swa_hr_delta = ((swa_hr_shift_plus - swa_hr_shift_minus))# / swa_hr_norm) * 100
+# # swa_hr_delta_mean = swa_hr_delta.groupby(level=[1, 2]).mean()
+# swa_hr_delta_long = swa_hr_delta.reset_index()
+#
+# # Calculate NREM / hour
+# nrem_stages = ["NR", "N1"]
+# nrem_int_df = stage_df.isin(nrem_stages).astype(int)
+# nr_hourly_perc = nrem_int_df.groupby(
+#     level=0
+# ).resample(
+#     "H",
+#     level=1,
+#     loffset=OFFSET
+# ).mean() * 100
+# # nr_hourly_perc_mean = nr_hourly_perc.groupby(level=[1]).mean()
+# nr_hourly_perc_long = nr_hourly_perc.stack(
+# ).reorder_levels(
+#     [0, 2, 1]
+# ).sort_index(
+# ).reset_index()
+#
+# # Calculate Wake / hour
+# wake_stages = ["W", "W1"]
+# wake_int_df = stage_df.isin(wake_stages).astype(int)
+# wake_hourly_perc = wake_int_df.groupby(
+#     level=0
+# ).resample(
+#     "H",
+#     level=1,
+#     loffset=OFFSET
+# ).mean() * 100
+# # wake_hourly_perc_mean = wake_hourly_perc.groupby(level=[1]).mean()
+# wake_hourly_perc_long = wake_hourly_perc.stack(
+# ).reorder_levels(
+#     [0, 2, 1]
+# ).sort_index(
+# ).reset_index()
+#
+# # Regression NREM ~ delta swa
+# swa_diss_df = swa_hr_delta_long.copy()
+# swa_diss_df["NR"] = nr_hourly_perc_long[nr_hourly_perc_long.columns[-1]]
+#
+# # Regression wake ~ delta swa
+# swa_accum_df = swa_hr_delta_long.copy()
+# swa_accum_df["Wake"] = wake_hourly_perc_long[wake_hourly_perc_long.columns[-1]]
 
 # Stats ########################################################################
 
-# log transformed
-# log_wake = np.log10(wake_spectrum)
-# log_nrem = np.log10(nrem_spectrum)
-#
-# # 1. Does the day affect the spectrum?
-# # repeated measures 2 way anova of power ~ freq x light period
-#
-# ders = log_wake.index.get_level_values(2).unique()
-# freqs = log_wake.columns
-# stats_colnames = ["Animal", "Time", "Derivation", "Frequency", "Power"]
-# dep_var = stats_colnames[-1]
-# day_col = stats_colnames[1]
-# freq_col = stats_colnames[3]
-# anim_col = stats_colnames[0]
-# der_col = stats_colnames[2]
-# data_list = [log_nrem, log_wake]
-# data_type = ["NREM", "Wake"]
-#
-# save_test_dir = pathlib.Path("/Users/angusfisk/Documents/01_PhD_files/"
-#                              "01_projects/01_thesisdata/03_lleeg/"
-#                              "03_analysis_outputs/05_figures/00_csvs/04_fig4")
-#
-# for df_type, label in zip(data_list, data_type):
-#     print(label)
-#     test_df = df_type.stack().reset_index()
-#     test_df.columns = stats_colnames
-#     type_dir = save_test_dir / label
-#     for der in ders:
-#         print(der)
-#         der_dir = type_dir / der
-#         der_df = test_df.query("%s == '%s'"%(der_col, der))
-#         der_df = der_df.drop(der_col, axis=1)
-#
-#         # rm anova
-#         anova = pg.rm_anova2(dv=dep_var,
-#                              within=[day_col, freq_col],
-#                              subject=anim_col,
-#                              data=der_df)
-#         pg.print_table(anova)
-#         anova_file = der_dir / "01_anova.csv"
-#         anova.to_csv(anova_file)
-#
-#         # post hoc
-#         ph_dict = {}
-#         for freq in freqs:
-#             print(freq)
-#             freq_df = der_df.query("%s == '%s'"%(freq_col, freq))
-#             ph = pg.pairwise_tukey(dv=dep_var,
-#                                    between=day_col,
-#                                    data=freq_df)
-#             pg.print_table(ph)
-#             ph_dict[freq] = ph
-#         ph_df = pd.concat(ph_dict)
-#         ph_file = der_dir / "02_posthoc.csv"
-#         ph_df.to_csv(ph_file)
+save_test_dir = pathlib.Path(
+    "/Users/angusfisk/Documents/01_PhD_files/01_projects/01_thesisdata"
+    "/03_lleeg/03_analysis_outputs/05_figures/00_csvs/04_fig4"
+)
+
+# Q1 Does power of the spectrum change between days
+# Repeated two way ANOVA of Power ~ Freq*days | anim
+
+stats_spec_df = nrem_mean.copy()
+stats_spec_df.index = stats_spec_df.index.droplevel(2)
+stats_spec_df = stats_spec_df.stack().reset_index()
+
+anim_col = stats_spec_df.columns[0]
+day_col = stats_spec_df.columns[1]
+freq_col = stats_spec_df.columns[2]
+power_col = stats_spec_df.columns[3]
+
+spec_rm = pg.mixed_anova(
+    dv=power_col,
+    within=day_col,
+    between=freq_col,
+    subject=anim_col,
+    data=stats_spec_df
+)
+pg.print_table(spec_rm)
+
+spec_name = save_test_dir / "01_spec_anova"
+spec_rm.to_csv(spec_name)
+
+# Q2 Does the Number of episodes change between day?
+# Rpeated two way anova of Count ~ Time*day | anim
+
+count_stats_df = long_frag.copy()
+
+anim_col = count_stats_df.columns[0]
+time_col = count_stats_df.columns[1]
+day_col = count_stats_df.columns[2]
+count_col = count_stats_df.columns[3]
+mean_col = count_stats_df.columns[4]
+
+count_rm = pg.rm_anova2(
+    dv=count_col,
+    within=[day_col, time_col],
+    subject=anim_col,
+    data=count_stats_df
+)
+pg.print_table(count_rm)
+
+count_rm_name = save_test_dir / "02_count_rm"
+count_rm.to_csv(count_rm_name)
+
+count_ph = aprep.tukey_pairwise_ph(
+    count_stats_df,
+    hour_col=time_col,
+    dep_var=count_col,
+    protocol_col=day_col
+)
+count_ph_name = save_test_dir / "02_count_ph"
+count_ph.to_csv(count_ph_name)
+
+# Q3 Does duration of episodes change between day?
+# repeated two way anova of duration ~ Time*day | anim
+
+mean_rm = pg.rm_anova2(
+    dv=mean_col,
+    within=[day_col, time_col],
+    subject=anim_col,
+    data=count_stats_df
+)
+pg.print_table(mean_rm)
+mean_name = save_test_dir / "03_mean_rm"
+mean_rm.to_csv(mean_name)
 
 
-# Step 6 Plot wake on top two and nrem on bottom two subplots
-
-# tidy data further - remove _L1 and baselinse period
-# baseline_wake_mask = long_wake["Light_period"] == "Baseline_-0"
-# long_wake = long_wake.mask(baseline_wake_mask)
-# ll1_wake_mask = long_wake["Animal"] == "LL1"
-# long_wake = long_wake.mask(ll1_wake_mask)
-
-# baseline_nrem_mask = long_nrem["Light_period"] == "Baseline_-0"
-# long_nrem = long_nrem.mask(baseline_nrem_mask)
-# ll1_nrem_mask = long_nrem['Animal'] == "LL1"
-# long_nrem = long_nrem.mask(ll1_nrem_mask)
-
-
+# Plotting #####################################################################
 # figure constants
 anim_col = cols[0]
-animals = long_nrem[anim_col].unique()
+# animals = long_nrem[anim_col].unique()
 lights = cols[1]
 ders = cols[2]
 freq = cols[3]
 power = cols[4]
-derivations = long_nrem[ders].unique()[1:]
+# derivations = long_nrem[ders].unique()[1:]
 frag_data_types = ["Count", "Mean"]
 time_col = frag_cols[1]
 day_col = frag_cols[2]
@@ -340,21 +350,35 @@ spectrum_grid = gs.GridSpec(
     nrows=1,
     figure=fig,
     right=0.45,
-    bottom=0.55
 )
 spectrum_axes = [plt.subplot(x) for x in spectrum_grid]
 
+days = long_nrem[lights].unique()
 curr_ax_spec = spectrum_axes[0]
-sns.pointplot(
-    x=freq,
-    y=power,
-    hue=lights,
-    data=long_nrem,
-    ax=curr_ax_spec,
-    capsize=capsize,
-    ci=sem,
-    errwidth=errwidth
-)
+for curr_day in days:
+    curr_day_data = long_nrem.query("%s == '%s'" %(lights, curr_day))
+    
+    curr_ax_spec.plot(
+        curr_day_data[freq],
+        curr_day_data[power]
+    )
+    curr_ax_spec.fill_between(
+        curr_day_data[freq],
+        curr_day_data[power] - curr_day_data["SEM"],
+        curr_day_data[power] + curr_day_data["SEM"],
+        alpha=0.5
+    )
+    
+# sns.pointplot(
+#     x=freq,
+#     y=power,
+#     hue=lights,
+#     data=long_nrem,
+#     ax=curr_ax_spec,
+#     capsize=capsize,
+#     ci=sem,
+#     errwidth=errwidth
+# )
 curr_ax_spec.set_yscale("log")
 spec_leg = curr_ax_spec.legend()
 spec_leg.remove()
@@ -368,8 +392,9 @@ curr_ax_spec.set_xticklabels(
     ha='right',
     size=labelsize
 )
-curr_ax_spec.xaxis.grid()
-plt.setp(curr_ax_spec.collections, facecolors='none')
+# curr_ax_spec.xaxis.grid()
+# curr_ax_spec.yaxis.grid()
+# plt.setp(curr_ax_spec.collections, facecolors='none')
 fig.text(
     0.5,
     1.1,
@@ -383,9 +408,8 @@ frag_grid = gs.GridSpec(
     nrows=2,
     ncols=1,
     figure=fig,
-    hspace=0,
+    hspace=0.1,
     left=0.55,
-    bottom=0.55
 )
 frag_axes = [plt.subplot(x) for x in frag_grid]
 
@@ -419,47 +443,56 @@ curr_ax_frag.set_xticklabels(
 fig.text(
     0.5,
     1.1,
-    "Wake Episodes",
+    "NREM Episodes",
     transform=frag_axes[0].transAxes,
     ha='center'
 )
 frag_axes[1].set_ylabel("Mean Duration, minutes")
-    
-# fig.autofmt_xdate()
 
-# reg constants
-swa_delta_col = swa_diss_df.columns[-2]
-nr_col = swa_diss_df.columns[-1]
-wake_col = swa_accum_df.columns[-1]
-anim_col = swa_diss_df.columns[0]
-day_col_reg = swa_diss_df.columns[1]
-time_col = swa_diss_df.columns[2]
-ylabel_reg = "Delta SWA, percent change -1 to +1 hr"
+# add stats to count levels
 
-reg_grid = gs.GridSpec(
-    nrows=1,
-    ncols=2,
-    figure=fig,
-    top=0.45
+# adjust y to accommodate
+ylevel_day1 = 0.9
+ylevel_day2 = 0.95
+
+count_axis = frag_axes[0]
+count_axis.set_ylim([0, 40])
+
+# Get y level
+ycoord_day1 = plot.sig_line_coord_get(
+    count_axis,
+    ylevel_day1
 )
-reg_axes = [plt.subplot(x) for x in reg_grid]
+ycoord_day2 = plot.sig_line_coord_get(
+    count_axis,
+    ylevel_day2
+)
 
-nr_wake_cols = [nr_col, wake_col]
-reg_ax_dict = dict(zip(nr_wake_cols, reg_axes))
-swa_data = dict(zip(nr_wake_cols, [swa_diss_df, swa_accum_df]))
-for data_type in nr_wake_cols:
-    curr_ax_reg = reg_ax_dict[data_type]
-    curr_data_reg = swa_data[data_type]
-    sns.scatterplot(
-        x=data_type,
-        y=swa_delta_col,
-        hue=day_col_reg,
-        data=curr_data_reg,
-        ax=curr_ax_reg
-    )
-    curr_ax_reg.legend().remove()
-    curr_ax_reg.set_ylabel(ylabel_reg)
-    curr_ax_reg.set_xlabel("%s, percent per hour"%(data_type))
+# get xvals where sig
+sig_day1 = [x.strftime("%H:%M") for x in plot.sig_locs_get(count_ph)]
+sig_day2 = [x.strftime("%H:%M")
+            for x in plot.sig_locs_get(count_ph,index_level2val=1)]
+
+label_loc_dict = plot.get_xtick_dict(frag_axes[1])
+
+plot.draw_sighlines(
+    yval=ycoord_day1,
+    sig_list=sig_day1,
+    label_loc_dict=label_loc_dict,
+    minus_val=0.5,
+    plus_val=0.5,
+    curr_ax=count_axis,
+    color="C1"
+)
+plot.draw_sighlines(
+    yval=ycoord_day2,
+    sig_list=sig_day2,
+    label_loc_dict=label_loc_dict,
+    minus_val=0.5,
+    plus_val=0.5,
+    curr_ax=count_axis,
+    color="C1"
+)
 
 # figure legend
 frag_axes[0].legend(
